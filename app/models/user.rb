bb68e32f741b540bfa2aaa069f51fa9c5f96785d
shofny <sho.fuyuyado0705@gmail.com>
2020-10-14 18:05:07 +0900

    追加、修正、rubocopを通した

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         validates :nickname, presence: true
-         validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
-         validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
-         validates :first_name_kana, presence: true, format: { with: /\A[ァ-ン]+\z/ }
-         validates :last_name_kana, presence: true, format: { with: /\A[ァ-ン]+\z/ }
-         validates :birthday, presence: true
-
+  validates :nickname, presence: true
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ン]+\z/ }
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ン]+\z/ }
+  validates :birthday, presence: true
 end
