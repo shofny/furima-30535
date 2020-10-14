62eb4df86c42cf06b3e9dab79f726895bd62bd80
shofny <sho.fuyuyado0705@gmail.com>
2020-10-14 19:26:52 +0900

    FactoryBotをFaker,Gimeiを用いて作成

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,10 +1,16 @@
 FactoryBot.define do
   factory :user do
-    nickname              {Faker::Name.initials(number: 2)}
-    email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password(min_length: 6)}
-    password_confirmation {password}
-    
-    
+    transient do
+      person { Gimei.name }
+    end
+      nickname              {Faker::Name.initials(number: 2)}
+      email                 {Faker::Internet.free_email}
+      password              {Faker::Internet.password(min_length: 6)}
+      password_confirmation {password}
+      first_name            { person.last.kanji }
+      last_name             { person.first.kanji }
+      first_name_kana       { person.last.katakana }
+      last_name_kana        { person.first.katakana }
+      birthday              { Faker::Date.between(from: '1930-01-01', to: '2015-12-31')}
   end
 end
