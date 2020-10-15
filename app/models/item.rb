d8d108610f2a6cdba60f599b730ef0e9d71c28b4
shofny <sho.fuyuyado0705@gmail.com>
2020-10-15 18:29:21 +0900

    active_hash用のモデル作成、アソシエーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,11 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
 
-
-
+  belongs_to_active_hash :category
+  belongs_to_active_hash :status
+  belongs_to_active_hash :shipping_fee
+  belongs_to_active_hash :delivery_source
+  belongs_to_active_hash :delivery_days
 
   belongs_to :user
   has_one_attached :image
