7ee78d32379996f796bcff5eb1f8ebfe04bfbe7f
shofny <sho.fuyuyado0705@gmail.com>
2020-10-15 12:40:53 +0900

    複数使用の正規表現を変数化

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  name_check = /\A[ぁ-んァ-ン一-龥]+\z/
+  name_check_kana = /\A[ァ-ン]+\z/
+  
   validates :nickname, presence: true
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
-  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ン]+\z/ }
-  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ン]+\z/ }
+  validates :first_name, presence: true, format: { with: name_check }
+  validates :last_name, presence: true, format: { with: name_check }
+  validates :first_name_kana, presence: true, format: { with: name_check_kana }
+  validates :last_name_kana, presence: true, format: { with: name_check_kana }
   validates :birthday, presence: true
 end
