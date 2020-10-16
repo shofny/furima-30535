2e745b739eba4d54f71f34005149cd38e5855f60
shofny <sho.fuyuyado0705@gmail.com>
2020-10-16 20:49:03 +0900

    FactoryBot,Fakerのランダム要素設定まで

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :item do
-    
+    name { Faker::Lorem.sentence }
+    description { Faker::Lorem.sentence }
+    category_id { Faker::Number.between(from: 2, to: 11) }
+    status_id { Faker::Number.between(from: 2, to: 7) }
+    shipping_fee_id { Faker::Number.between(from: 2, to: 3) }
+    delivery_source_id { Faker::Number.between(from: 2, to: 48) }
+    delivery_days_id { Faker::Number.between(from: 2, to: 4) }
+    price { Faker::Number.between(from: 300, to: 9999999) }
+    association :user
   end
 end
