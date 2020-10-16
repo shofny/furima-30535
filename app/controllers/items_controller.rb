0d1c1d9edcfe107ecd133a2c6aae92f119f61fa8
shofny <sho.fuyuyado0705@gmail.com>
2020-10-16 11:26:01 +0900

    new,createアクション設置、MySQLに保存されることまで確認

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,9 +4,18 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.create(params_item)
+    redirect_to root_path
   end
 
+
+  private
+
+  def params_item
+    params.require(:item).permit(:name, :description, :image, :category_id, :status_id, :shipping_fee_id, :delivery_source_id, :delivery_days_id, :price).merge(user_id: current_user.id)
+  end
 end
