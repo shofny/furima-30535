fa110c8a95589c6b9d01f8b82e8bf85c559a5282
shofny <sho.fuyuyado0705@gmail.com>
2020-10-20 16:05:58 +0900

    商品詳細ページに各情報が記載されるようにした

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -15,10 +15,10 @@ class ItemsController < ApplicationController
     else
       render :new
     end
+  end
 
-    def show
-    end
-
+  def show
+    @item = Item.find(params[:id])
   end
 
   private
