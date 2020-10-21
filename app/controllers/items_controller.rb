2cae5c0caf563f97771f3f692377de88430f8ef1
shofny <sho.fuyuyado0705@gmail.com>
2020-10-21 16:31:06 +0900

    rubocopを実行

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -45,9 +45,6 @@ class ItemsController < ApplicationController
 
   def go_to_index
     @item = Item.find(params[:id])
-    unless current_user.id == @item.user.id
-      redirect_to action: :index
-    end
+    redirect_to action: :index unless current_user.id == @item.user.id
   end
-
 end
