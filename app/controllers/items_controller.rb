47f287e0dddd9392f001e93903829291076c4616
shofny <sho.fuyuyado0705@gmail.com>
2020-10-21 19:30:36 +0900

    指摘事項の修正-3

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -39,8 +39,7 @@ class ItemsController < ApplicationController
       @item.destroy
       redirect_to root_path
     else
-      @items = Item.all.order('created_at DESC')
-      render :index
+      render :show
     end
   end
 
