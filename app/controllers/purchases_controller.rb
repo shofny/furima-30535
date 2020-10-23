debd4c2da1bfb6dc51779474994225fde9c8e5f8
shofny <sho.fuyuyado0705@gmail.com>
2020-10-23 19:30:02 +0900

    バリテーション設置、エラー文の表示

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -8,15 +8,18 @@ class PurchasesController < ApplicationController
 
   def create
     @user_purchase = UserPurchase.new(params_purchase)
-    binding.pry
+      if @user_purchase.valid?
     @user_purchase.save
     redirect_to root_path
+      else
+    @item = Item.find(params[:item_id])
+    render action: :index
+      end
   end
 
   private
 
   def params_purchase
-    binding.pry
     params.require(:user_purchase).permit(:postal_code, :prefecuture_id, :city, :address, :build_name, :tel).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 
