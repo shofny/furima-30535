4408857ca6a5dec09a7080df01b841fef4c17c79
shofny <sho.fuyuyado0705@gmail.com>
2020-10-26 18:24:36 +0900

    PAY.JPからからトークンが帰ってくることを確認

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,10 +1,26 @@
 const pay = () => {
-  const form = document.getElementById("charge-form")
+  Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);
+  const form = document.getElementById("charge-form");
   form.addEventListener("submit", (e) =>{
-    e.preventDefault()
-    console.log("test")
+    e.preventDefault();
+    
+    const formResult = document.getElementById("charge-form");
+    const formData = new FormData(formResult);
 
-  })
+    const card = {
+      number: formData.get("user_purchase[number]"),
+      cvc: formData.get("user_purchase[cvc]"),
+      exp_month: formData.get("user_purchase[month]"),
+      exp_year: `20${formData.get("user_purchase[year]")}`,
+    };
+
+    Payjp.createToken(card, (status,response) => {
+      if (status == 200) {
+        const token = response.id
+        console.log(token)
+      }
+    });
+  });
 };
 
 window.addEventListener("load", pay);
